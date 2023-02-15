672743f079a8c54985f0c3c759be3fd51cc340bf
samuel.bowden <samuel.bowden@rights-s.jp>
2023-02-15 11:14:50 +0900

    詳細検索機能を追加 ・Advanced searchボタン/メニューを追加 ・クッキーでメニューの状態を覚える ・CSSを調整

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,11 +8,15 @@ class Article < ApplicationRecord
     validates :image_link, length: { maximum: 1000 }
 
     def self.ransackable_attributes(auth_object = nil)
-        %w(title body)
+        %w(title body language)
     end
 
     def self.ransackable_associations(auth_object = nil)
         %w(articles comments)
     end
+
+    ransacker :created_at do
+        Arel.sql('date(created_at)')
+      end
 
 end 
